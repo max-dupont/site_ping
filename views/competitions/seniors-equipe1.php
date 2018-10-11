4f5e8f3b84725b5496c43c1ad83bb4e74c49bd79
max-dupont <maxime.dupont49280@gmail.com>
2018-10-11 21:46:17 +0200

    change url compétitions

--- views/competitions/seniors-equipe1.php
+++ views/competitions/seniors-equipe1.php
@@ -1,5 +1,5 @@
 <div id="right">
     <div id="right_row1">
-        <embed src="http://www.fftt.com/sportif/chpt_equipe/chp_div.php?organisme_pere=49&cx_poule=3289&D1=3053&virtuel=0" ></embed>
+        <embed src="http://www.fftt.com/sportif/chpt_equipe/chp_div.php?organisme_pere=49&cx_poule=3285&D1=3052&virtuel=0" ></embed>
     </div>
 </div>
--- views/competitions/seniors-equipe4.php
+++ views/competitions/seniors-equipe4.php
@@ -1,1 +1,5 @@
-<embed src="http://www.fftt.com/sportif/chpt_equipe/chp_div.php?organisme_pere=49&cx_poule=3289&D1=3053&virtuel=0" ></embed>
+<div id="right">
+    <div id="right_row1">
+        <embed src="http://www.fftt.com/sportif/chpt_equipe/chp_div.php?organisme_pere=49&cx_poule=3298&D1=3053&virtuel=0" ></embed>
+    </div>
+</div>
